70bbea62f983f2958ab74d34aefc91ddc0f0dd36
KevinMartinezMella <iamkamm7@gmail.com>
2023-07-01 23:07:35 -0400

    Proyecto Base

---
+++ apps/master/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class PerfilUsuario(models.Model):
+    nombrePerfil = models.CharField(max_length=50)
+
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    email = models.CharField(max_length=80)
+    perfil = models.ForeignKey("PerfilUsuario", on_delete=models.CASCADE)
+    password = models.CharField(max_length=50)
+
+class Categoria(models.Model):
+    nombreCategoria = models.CharField(max_length=50)
+
+class Producto(models.Model):
+    nombreProducto = models.CharField(max_length=50)
+    categoriaProducto = models.ForeignKey("Categoria", on_delete=models.CASCADE)
+    precioProducto = models.IntegerField()
+
+class Venta(models.Model):
+    cliente = models.ForeignKey("Usuario", on_delete=models.CASCADE)
+    productos = models.ManyToManyField("Producto")
+    fechaVenta = models.DateTimeField(auto_now_add=True)
